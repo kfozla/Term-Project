3ca857b1f2dc7ce69e3b307afe8871fc3ddf4c1b
Yusuf Gökdağ <yusuf@yusuf-MBP.home>
2024-04-19 00:58:17 +0300

    implemented Z-Chart and Loss Function Table also code algorithm to iterate optimal lot size and reorder point

---
+++ src/main/java/Application.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Application {
+    //INITIALIZING Z CHART READER CLASS
+    static ZChartReader reader=new ZChartReader("z-chart.txt");
+
+    //CALCULATING HOLDING COST AND ANNUAL DEMAND
+    public static float CalculateHoldingCost(float UnitCost, float InterestRate){
+        return UnitCost*InterestRate/100;
+    }
+    public static float CalculateAnnualDemand(float LeadTime,float LeadTimeDemand){
+        return (LeadTimeDemand/LeadTime)*(12/LeadTime);
+    }
+
+    public static void main(String[] args) {
+        //USER INPUT FOR VARIABLES
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Enter Unit Cost: ");
+        float UnitCost = scanner.nextFloat();
+        System.out.println("Enter Ordering Cost: ");
+        float OrderingCost = scanner.nextFloat();
+        System.out.println("Enter Penalty Cost: ");
+        float PenaltyCost = scanner.nextFloat();
+        System.out.println("Enter Annual Interest Rate: ");
+        float InterestRate = scanner.nextFloat();
+        System.out.println("Enter Lead Time: ");
+        float LeadTime = scanner.nextFloat();
+        System.out.println("Enter Lead Time Demand: ");
+        float LeadTimeDemand = scanner.nextFloat();
+        System.out.println("Enter Lead Time Standard Deviation: ");
+        float LeadTimeStandardDeviation = scanner.nextFloat();
+
+        float HoldingCost =CalculateHoldingCost(UnitCost,InterestRate);
+        float AnnualDemand = CalculateAnnualDemand(LeadTime,LeadTimeDemand);
+
+        //HARDCODED THE FIRST ITERATION FOR Q0 VALUE
+        double Q0= Math.sqrt((2*OrderingCost*(12/LeadTime*LeadTimeDemand))/HoldingCost);
+        double FR0=1-Q0*HoldingCost/(PenaltyCost*(12/LeadTime*LeadTimeDemand));
+        double zValue=reader.getZValueFromF(FR0);
+        double R0=zValue*OrderingCost+LeadTimeDemand;
+        double nR0= reader.getLValueFromF(FR0)*OrderingCost;
+
+        int counter=1;
+
+        double Q=0;
+        double FR;
+        double zedValue;
+        double R=0;
+        double nR=nR0;
+        double Qex=Q0;
+        //ITERATION FOR OPTIMAL LOT SIZE AND REORDER POINT
+        while (Q!=Qex){
+            Qex=Q;
+            Q=Math.sqrt(2*((12/LeadTime*LeadTimeDemand)*(OrderingCost+PenaltyCost*nR)/HoldingCost));
+            FR=1-Q*HoldingCost/(PenaltyCost*(12/LeadTime*LeadTimeDemand));
+            zedValue=reader.getZValueFromF(FR);
+            R=zedValue*OrderingCost+LeadTimeDemand;
+            nR=reader.getLValueFromF(FR)*OrderingCost;
+            counter++;
+        }
+        System.out.println("Optimal Lot Size: "+Q);
+        System.out.println("Reorder Point: "+R);
+        System.out.println("Holding Cost: "+ HoldingCost);
+        System.out.println("Annual Demand: "+ AnnualDemand);
+        System.out.println("Total Iteration: "+counter);
+
+    }
+
+
+
+}
---
+++ src/main/java/ZChartReader.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZChartReader {
+    private final List<Double> zValues=new ArrayList<>();
+    private final List<Double> fValues=new ArrayList<>();
+    private final List<Double> lValues=new ArrayList<>();
+
+    public ZChartReader(String filePath){
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] tokens = line.split("\\s+");
+                Double z = Double.parseDouble(tokens[0]);
+                Double f = Double.parseDouble(tokens[1]);
+                Double l = Double.parseDouble(tokens[2]);
+                zValues.add(z);
+                fValues.add(f);
+                lValues.add(l);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public Double getZValueFromF(Double inputValue){
+        double minDifference=Math.abs(inputValue-fValues.get(0));
+        double difference;
+        int index = 0;
+        for (int i=0;i<fValues.size();i++){
+            difference=Math.abs(inputValue-fValues.get(i));
+            if(difference<minDifference){
+                minDifference=difference;
+                index=i;
+            }
+        }
+        return zValues.get(index);
+    }
+
+    public Double getLValueFromF(Double inputValue){
+        double minDifference=Math.abs(inputValue-fValues.get(0));
+        double difference;
+        int index = 0;
+        for (int i=0;i<fValues.size();i++){
+            difference=Math.abs(inputValue-fValues.get(i));
+            if(difference<minDifference){
+                minDifference=difference;
+                index=i;
+            }
+        }
+        return lValues.get(index);
+    }
+}
